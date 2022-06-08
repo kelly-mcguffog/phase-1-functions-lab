e62c46a3eb4678d7c7f3a45f51c740241a380a0c
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-08 10:11:35 -0400

    commit tree 3c5ae174d3d5f75867505f71d63e657690aa47e0

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if(someValue >=42){
+    return someValue - 42;
+    } else {
+        return 42 - someValue;
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination)) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start, destination) <= 400){
+        return distanceTravelledInFeet(start, destination) * 0;
+    } else if(400 < distanceTravelledInFeet(start, destination) && distanceTravelledInFeet(start, destination) < 2000){
+        return (distanceTravelledInFeet(start, destination)-400) * 0.02;
+    } else if(2000 < distanceTravelledInFeet(start, destination) && distanceTravelledInFeet(start, destination) < 2500){
+        return 25;
+    } else if(distanceTravelledInFeet(start, destination) > 2500){
+        return 'cannot travel that far';
+    }
+}
